1df117bca7473f84fe27dfa02cd52d9855a1d832
efarinholt <evan@fireinspace.net>
2014-10-29 15:23:56 -0400

    Add indexed class to element

---
+++ indexedClasses.js
@@ -0,0 +1,6 @@
+// loop through all <tr class="q"> tags and add indexed class to each, <tr class="q_1">
+$(function () { 
+	for(var index = 0; index < $('tr').length; index++){
+		$('tr.q').eq(index).addClass('q_' + index);
+	}
+});
