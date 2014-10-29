eb897b1b079ccd1971a6befb4e85f90299710173
Evan Farinholt <evan@fireinspace.net>
2014-10-29 16:50:29 -0400

    Update indexedClasses.js

--- indexedClasses.js
+++ indexedClasses.js
@@ -1,4 +1,4 @@
-// loop through all <tr class="q"> tags and add indexed class to each, <tr class="q_1">
+// loop through all <tr class="q"> tags and add indexed class to each, ex.: <tr class="q_1">
 $(function () { 
 	for(var index = 0; index < $('tr').length; index++){
 		$('tr.q').eq(index).addClass('q_' + index);
